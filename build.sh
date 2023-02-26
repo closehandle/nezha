1891d6e954944cfe69509c40682f6d408cbb5c92
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-02-27 03:55:23 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+rm -fr release && mkdir release
+
+git clone https://github.com/naiba/nezha || exit $?
+cd nezha
+
+go build -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-agent cmd/agent || exit $?
+go build -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-dashboard cmd/dashboard || exit $?
+
+mv -f nezha-agent ../release
+mv -f nazha-dashboard ../release
+exit 0
