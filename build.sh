e5866919cc2a0e8b3453e84d5906f6922a50dc9a
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-02-27 04:04:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,15 @@ rm -fr release && mkdir release
 git clone https://github.com/naiba/nezha || exit $?
 cd nezha
 
+export CGO_ENABLED='0'
+export GOROOT_FINAL='/usr'
+export GOOS='windows'
+export GOARCH='amd64'
+
 go get -d -t -u -v ./... || exit $?
 go mod tidy || exit $?
-go build -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-agent ./cmd/agent || exit $?
-go build -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-dashboard ./cmd/dashboard || exit $?
+go build -v -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-agent ./cmd/agent || exit $?
+go build -v -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-dashboard ./cmd/dashboard || exit $?
 
 mv -f nezha-agent ../release
 mv -f nezha-dashboard ../release
