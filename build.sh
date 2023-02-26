e112e43bdd91d5182f9fe219c49602ba2ddefc03
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-02-27 04:06:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ export GOARCH='amd64'
 
 go get -d -t -u -v ./... || exit $?
 go mod tidy || exit $?
-go build -v -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-agent ./cmd/agent || exit $?
-go build -v -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-dashboard ./cmd/dashboard || exit $?
+go build -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-agent ./cmd/agent || exit $?
+go build -trimpath -asmflags '-s -w' -ldflags '-s -w' -o nezha-dashboard ./cmd/dashboard || exit $?
 
 mv -f nezha-agent ../release
 mv -f nezha-dashboard ../release
